dd750706975213e70e704d0cec21caafe6ca6b34
Mike Zevitas <mzevitas@gmail.com>
2015-08-27 14:51:06 -0400

    connected mailing list to parse

---
+++ app/scripts/mailing/mailing.controller.js
@@ -0,0 +1,22 @@
+;(function (){
+
+  'use strict';
+
+  angular.module('app')
+    .controller('MailCtrl', ['$scope', '$http', '$state', 'PARSE', 'MailFactory', '$cacheFactory',
+    function ($scope, $http, $state, PARSE, MailFactory,  $cacheFactory){
+
+       var cache = $cacheFactory.get('$http');
+
+     
+
+      $scope.addMail = function (mailObj) {
+        $scope.mails = {};
+        MailFactory.add(mailObj).success( function (results) {
+          mailObj.objectId = results.objectId;
+          cache.remove(PARSE.URL + 'classes/mail');
+        
+        });
+      };
+    }]);
+}());
---
+++ app/scripts/mailing/mailing.factory.js
@@ -0,0 +1,27 @@
+;(function() {
+
+	'use strict';
+
+    angular.module('app')
+        .factory('MailFactory', ['$http', '$state', 'PARSE', 
+            function($http, $state, PARSE) {
+
+
+
+        var addMail = function(mailObj) {
+            return $http.post(PARSE.URL + 'classes/mail', mailObj, PARSE.CONFIG)
+                .success(function() {
+                    $state.go('portal');
+
+                });
+        };
+
+
+                return {
+                    add: addMail
+
+                };
+
+            }
+        ]);
+}());
